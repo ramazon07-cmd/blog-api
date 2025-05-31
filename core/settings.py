27f2048b8a3a1e8961f4b46b54f3e2e693e88a2c
ramazon07-cmd <ustau2453@gmail.com>
2025-05-31 09:38:16 +0500

    jj

--- core/settings.py
+++ core/settings.py
@@ -13,13 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uhqez7($ql*uycs-g=0&0d9hs=6mu8zo=#jr&xno^5x$pyj0+6"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
